29fb4bd88a91fd238ef6decd3d0e7b2ed9036185
76500833 <145300587+76500833@users.noreply.github.com>
2023-11-22 17:42:25 -0500

    safety commit

--- Assets/index.js
+++ Assets/index.js
@@ -1,6 +1,10 @@
 var startButton = $('#start')
 var secondsLeft = 10;
 var timer = $('#timer')
+questions = {
+  []
+}
+
 function setTime(){
   var timerInterval = setInterval(function(){
     secondsLeft--;
@@ -8,18 +12,27 @@ function setTime(){
 
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
-      return()
+      return
     }
+  
+  //! Do you reccomend writing more functionality
+  //! here? The rest of the game in the timer function?
+  
   }, 1000);
+
+//TODO Look for any answer element htat has an attribute that is equal to true
 }
-startButton.on('click', function () {
-  setTime()
-  //TODO display question and answers
-    //TODO in this function you need to save each click
-    //TODO if the click is correct, do nothing
-    //TODO if incorrect, subtract time
-    //TODO if time reaches zero, take to home page
-    //TODO if time is not zero, store time in localStorage
-    //TODO store users name from an input box in local storage
-    //TODO print both on the home page
+startButton.on('click', function () { //! STARTS TIMER WHEN CLICKED
+  setTime() 
 });
+
+
+
+  //TODO display question and answers
+    //TODO in this function you need to check each click for:
+      //TODO if the click is correct, do take to next question
+      //TODO if incorrect, subtract time, take to next question
+    //TODO if time reaches zero, take to home page
+    //TODO store users name from an input box in local storage + their time
+    //TODO print both on the home page
+
