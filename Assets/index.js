bb63fe3c03204e7cb54fb59185914d450578b369
76500833 <145300587+76500833@users.noreply.github.com>
2023-11-22 17:19:01 -0500

    created timer
    timer up and running

--- Assets/index.js
+++ Assets/index.js
@@ -1,117 +1,25 @@
-// * Start button:
-startButton = document.getElementById("startButton");
-
-// * Score:
-score = document.getElementById("score");
-
-//* Questions:
-questions = document.getElementById("questions");
-
-//* Answers:
-choices = document.getElementById("choice");
-
-nextButton = document.getElementById("nextButton");
-// ! Seconds left (begining with). This is used in the timer.
-secondsLeft = 40;
-
-// ! Listen for a click on the start button and then calls the setTime() function.
-function startGame() {
-  startButton.addEventListener("click", function () {
-    setTime();
-
-    updateQuestionAndChoices();
-  });
-}
-//! An event listener that updates currentQuestionIndex.
-nextButton.addEventListener('click', function() {
-  currentQuestionIndex++;
-  if (currentQuestionIndex < questionsAndAnswers.length) {
-      updateQuestionAndChoices();
-  } else {
-      console.log('No more questions');
-  }
-});
-// ! Timer that decreases the seconds left by using "setInterval".
-function setTime() {
-  var timerInterval = setInterval(function () {
-    // ! setInterval(function) calls a function or evaluates an expression at specified intervals (in milliseconds).
-    // ! Every second that passes, the secondsLeft variable is checked to see if it is equal to 0.
-    if (secondsLeft === 0) {
-      clearInterval(timerInterval);
-      return;
-    }
-    // ! Every second, the secondsLeft variable is decreased by one
+var startButton = $('#start')
+var secondsLeft = 10;
+var timer = $('#timer')
+function setTime(){
+  var timerInterval = setInterval(function(){
     secondsLeft--;
-    // ! and the textContent of the score element is updated.
-    score.textContent = "Time: " + secondsLeft;
+    timer.text(secondsLeft)
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      return()
+    }
   }, 1000);
 }
-
-var questionsAndAnswers = [
-    {
-        question: "What is an object?",
-        choices: ["A collection of keys and values", "a burger", "a minecraft", "a dog"],
-        answer: "A collection of keys and values"
-    },
-    {
-        question: "How do you center a div",
-        choices: ["justify-content: center;", "justify-content: smol but centered;", "align-items: mom's spaghetti", "text-align: center;"],
-        answer: "justify-content:center;"
-    },
-    {
-        question: "How do you store something in local storage?",
-        choices: ["localStorage.", "Berlin", "Paris", "Madrid"],
-        answer: "localStorage.setItem('key', 'value')"
-    }
-];
-
-//TODO Make a function that causes the questions and answers to populate.
-
-//* Generate 4 buttons Credit to co pilot for 61 -
-// Assuming you have a question element with this ID
-var questionElement = document.getElementById('question');
-
-// Assuming you have 4 buttons with these IDs in your HTML
-var buttons = [
-    document.getElementById('button1'),
-    document.getElementById('button2'),
-    document.getElementById('button3'),
-    document.getElementById('button4')
-];
-// buttons.addEventListener('click', function() {
-//   if (buttons.textContent === questionsAndAnswers.answer) {
-//     console.log("correct");
-//   }
-// });
-var currentQuestionIndex = 0;
-
-function updateQuestionAndChoices() {
-  var currentQuestion = questionsAndAnswers[currentQuestionIndex];
-  questionElement.textContent = currentQuestion.question;
-  for (var i = 0; i < currentQuestion.choices.length; i++) {
-    buttons[i].textContent = currentQuestion.choices[i];
-  }
-}
-
-
-
-// Assuming you have a "Next" button with this ID in your HTML
-
-// Call initially to set the first question and choices
-
-
-// // Add an event listener for the click event
-// button.addEventListener('click', function() {
-//     console.log('Button was clicked');
-// });
-
-// Add the button to the body of the document
-//* next button works. It goes to the next question.
-//TODO Make a function that checks the answers.
-//log user click
-//TODO Make a function that ends the game when the timer reaches 0.
-//TODO Implement a highscore system.
-//TODO Make a function that saves the highscore to local storage.
-//TODO Make a function that displays the highscore.
-
-startGame();
+startButton.on('click', function () {
+  setTime()
+  //TODO display question and answers
+    //TODO in this function you need to save each click
+    //TODO if the click is correct, do nothing
+    //TODO if incorrect, subtract time
+    //TODO if time reaches zero, take to home page
+    //TODO if time is not zero, store time in localStorage
+    //TODO store users name from an input box in local storage
+    //TODO print both on the home page
+});
