56ffa774bff3f7dc1a3295780dd9e168d41d750e
76500833 <145300587+76500833@users.noreply.github.com>
2023-11-22 23:57:31 -0500

    lots of work done!
    now checks answers, and goes to next question.

--- Assets/index.js
+++ Assets/index.js
@@ -1,38 +1,80 @@
-var startButton = $('#start')
+
+var startButton = $("#start");
 var secondsLeft = 10;
-var timer = $('#timer')
-questions = {
-  []
-}
+var timer = $("#timer");
+var questions = $('#questions')
 
-function setTime(){
-  var timerInterval = setInterval(function(){
+$('#questions').children().each(function(){
+  $(this).css('display', 'none')
+})
+
+function startTimer() {
+  $('#questions').children().each(function(){
+    $(this).css('display', 'block')
+  })
+  var timerInterval = setInterval(function () {
     secondsLeft--;
-    timer.text(secondsLeft)
+    timer.text(secondsLeft);
 
-    if(secondsLeft === 0) {
+    if (secondsLeft === 0) {
       clearInterval(timerInterval);
-      return
+      timer.text("");
     }
-  
-  //! Do you reccomend writing more functionality
-  //! here? The rest of the game in the timer function?
-  
   }, 1000);
-
-//TODO Look for any answer element htat has an attribute that is equal to true
 }
-startButton.on('click', function () { //! STARTS TIMER WHEN CLICKED
-  setTime() 
+questionsAndAnswers = {
+  questionOne: "This is question one",
+  answerOne: "This is answer one",
+
+  questionTwo: "This is quetsion two",
+  answerTwo: "This is answer Two",
+
+  questionThree: "This is question one",
+  answerThree: "This is answer one",
+
+  questionFour: "This is quetsion two",
+  answerFour: "This is answer Two",
+};
+const { questionOne, answerOne, questionTwo, answerTwo,
+questionThree, answerThree, questionFour, answerFour } = questionsAndAnswers;
+
+
+startButton.on("click", function () {
+  //* Timer
+  startTimer();
+  //* Displays questions/answers. Checks if user was correct/panalizes -2 seconds. 
+  displayQuestion()
+
 });
 
+function displayQuestion () {
+
+    var question = $("#question-text").text("What is HTML")
+    var answerOne = $("#a1").addClass('correct').text('hypertext markup language')
+    var answerTwo = $("#a2").addClass('wrong').text('hypertext monkey language')
+    var answerThree = $("#a3").addClass('wrong').text('hyper caffienated monkey dance')
+    var answerFour = $("#a4").addClass('wrong').text('hypertext money laundering')
+
+    //? Check if user click was correct
+    var button = $('button')
+    button.on('click', function(event){
+    //? If correct (display next question)
+      if($(this).hasClass("correct")){
+        //TODO if the click is correct, take to next question
+        var question = $("#question-text").text("What is CSS")
+        var answerOne = $("#a1").addClass('correct').text('A')
+        var answerTwo = $("#a2").addClass('wrong').text('B')
+        var answerThree = $("#a3").addClass('wrong').text('C')
+        var answerFour = $("#a4").addClass('wrong').text('D')
+      }
+      else {
+        secondsLeft = secondsLeft - 2
+      }
+})
+}
 
 
-  //TODO display question and answers
-    //TODO in this function you need to check each click for:
-      //TODO if the click is correct, do take to next question
-      //TODO if incorrect, subtract time, take to next question
-    //TODO if time reaches zero, take to home page
-    //TODO store users name from an input box in local storage + their time
-    //TODO print both on the home page
+//TODO if time reaches zero, take to home page
+//TODO store users name from an input box in local storage + their time
+//TODO print both on the home page
 
